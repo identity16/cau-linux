ef4ce953a137c17f12e3ca98108ae047e46ee700
identity16 <dnjswns0930@gmail.com>
2021-09-27 16:35:00 +0900

    Add validation

--- multi-table.sh
+++ multi-table.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+if [ $1 -ge 10 ] || [ $2 -ge 10 ] || [ $1 -le 0 ] || [ $2 -le 0 ]
+then
+    echo "Error: try with numbers in 1 ~ 9"
+    exit 1
+fi
+
 for i in $(seq 1 $1)
 do
     for j in $(seq 1 $2)
@@ -8,3 +14,5 @@ do
     done
     echo ""
 done
+
+exit 0
