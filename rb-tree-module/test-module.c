d4205126306a7b0a1aee3fadb40326da68e1f119
identity16 <dnjswns0930@gmail.com>
2021-11-10 04:30:44 +0900

    tmc

--- rb-tree-module/test-module.c
+++ rb-tree-module/test-module.c
@@ -104,25 +104,6 @@ int rb_delete(struct rb_root *mytree, int key) {
 	return FALSE;
 }
 
-unsigned long long calclock(struct timespec *spclock, unsigned long long *total_time, unsigned long long *total_count) {
-	long temp, temp_n;
-	unsigned long long timedelay = 0;
-
-	if (spclock[1].tv_nsec >= spclock[0].tv_nsec) {
-		temp = spclock[1].tv_sec - spclock[0].tv_sec;
-		temp_n = spclock[1].tv_nsec - spclock[0].tv_nsec;
-		timedelay = BILLION * temp + temp_n;
-	} else {
-		temp = spclock[1].tv_sec - spclock[0].tv_sec + 1;
-		temp_n = BILLION + spclock[1].tv_nsec - spclock[0].tv_nsec;
-		timedelay = BILLION * temp + temp_n;
-	}
-
-	__sync_fetch_and_add(total_time, timedelay);
-	__sync_fetch_and_add(total_count, 1);
-	return timedelay;
-}
-
 void add_to_rb_tree(int count) {
 	struct rb_root my_tree = RB_ROOT;
 	int i;
