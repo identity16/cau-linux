7ea644ab088a8b5d1c4cd52b572e896485b79a16
identity16 <dnjswns0930@gmail.com>
2021-11-30 22:12:22 +0900

    Implement: Spinlock linked list insertion

--- assignment-10/spinlock-module/spinlock-module.c
+++ assignment-10/spinlock-module/spinlock-module.c
@@ -6,17 +6,36 @@
 #include <linux/kthread.h>
 #include <linux/sched.h>
 #include <linux/delay.h>
+#include <linux/list.h>
+#include <linux/time.h>
 
 int counter;
 spinlock_t counter_lock;
 struct task_struct *thread1, *thread2;
 
+struct my_node {
+	struct list_head list;
+	int data;
+};
+
+struct list_head my_list;
+
 static int writer_function(void *data) {
     while(!kthread_should_stop()) {
         spin_lock(&counter_lock);
-        counter++;
-        printk("%s, counter: %d, pid: %u\n", __func__, counter, current->pid);
-        spin_unlock(&counter_lock);
+        if(counter < 100000){
+            counter++;
+            
+            struct my_node *new = kmalloc(sizeof(struct my_node), GFP_KERNEL);
+            new->data = counter;
+            list_add(&new->list, &my_list);
+
+            printk("%s, counter: %d, pid: %u\n", __func__, counter, current->pid);
+            spin_unlock(&counter_lock);
+        } else {
+            spin_unlock(&counter_lock);
+            break;
+        }
         msleep(500);
     }
     do_exit(0);
@@ -24,7 +43,11 @@ static int writer_function(void *data) {
 
 static int __init spinlock_mod_init(void) {
     printk("%s, Entering module\n", __func__);
+    
     counter = 0;
+    
+    INIT_LIST_HEAD(&my_list);
+
     spin_lock_init(&counter_lock);
     thread1 = kthread_run(writer_function, NULL, "writer_function");
     thread2 = kthread_run(writer_function, NULL, "writer_function");
