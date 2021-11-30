f85774282101ff570af8ba36feb1bf9a0ac98e5c
identity16 <dnjswns0930@gmail.com>
2021-11-30 13:42:12 +0900

    Implement: atomic actions

--- assignment-10/atomic-module/atomic-module.c
+++ assignment-10/atomic-module/atomic-module.c
@@ -2,9 +2,52 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
-int __init atomic_module_init(void) {
-    printk("Hello Module!\n");
+#define TRUE    1
+#define FALSE   0
 
+int running = FALSE;
+int grade = 0;
+
+int test_thread(void *_arg) {
+    int *arg = (int *)_arg;
+    int i, thread_grade;
+    
+    while(!kthread_should_stop()) {
+        // WAITING FOR NEXT GAME...
+        while(!__sync_lock_test_and_set(&running, running));
+        int sum = 0;
+
+        for(i=0; i<10000; i++) {
+            sum += i;
+        }
+
+        thread_grade = __sync_fetch_and_add(&grade, 1);
+        
+        printk("argument: %d, grade: %d\n", *arg, thread_grade);
+        if(thread_grade == 4) {
+            __sync_val_compare_and_swap(&thread_grade, 4, 0);
+            __sync_lock_test_and_set(&running, FALSE);
+        }
+    }
+
+    kfree(arg);
+    return 0;
+}
+
+void thread_create(void) {
+    int i;
+    /* thread create */
+    for(i=0; i<4; i++) {
+        int *arg = (int *)kmalloc(sizeof(int), GFP_KERNEL);
+        *arg = i;
+        kthread_run(&test_thread, (void *)arg, "test_thread");
+    }
+}
+
+int __init atomic_module_init(void) {
+    thread_create();
+
+    printk(KERN_EMERG "Hello Module!\n");
 	return 0;
 }
 
