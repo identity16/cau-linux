ceaf79b93c0df9c8a26ff34a81001f15cd63a75e
identity16 <dnjswns0930@gmail.com>
2021-12-03 21:05:13 +0900

    Modify: Integrate functions

--- term-project/linked-list-module/linked-list-module.c
+++ term-project/linked-list-module/linked-list-module.c
@@ -14,18 +14,11 @@ void search_from_linked_list(int count, int num);
 unsigned long long calclock(struct timespec *spclock, unsigned long long *total_time, unsigned long long *total_count);
 
 int __init linked_list_module_init(void) {
-	add_to_linked_list(1000);
-	add_to_linked_list(10000);
-	add_to_linked_list(100000);
-
-
-	search_from_linked_list(1000, 749);
-	search_from_linked_list(10000, 3711);
-	search_from_linked_list(100000, 86032);
-
-	remove_from_linked_list(1000);
-	remove_from_linked_list(10000);
-	remove_from_linked_list(100000);
+	search_from_linked_list(10000, 0);
+	search_from_linked_list(100000, 0);
+	search_from_linked_list(1000000, 0);
+	search_from_linked_list(10000000, 0);
+	search_from_linked_list(100000000, 0);
 
 	return 0;
 }
@@ -37,36 +30,6 @@ void __exit linked_list_module_cleanup(void) {
 module_init(linked_list_module_init);
 module_exit(linked_list_module_cleanup);
 MODULE_LICENSE("GPL");
-
-void add_to_linked_list(int count) {
-	struct list_node *my_list = kmalloc(sizeof(struct list_node), GFP_KERNEL);
-	int i;
-
-	/* Variables for time calcualtion */
-	struct timespec spclock[2];
-	unsigned long long list_time;
-	unsigned long long list_count;
-	unsigned long long delay;	
-	
-	/* Initialize List */
-	CAU_INIT_LIST_HEAD(my_list);
-
-	getnstimeofday(&spclock[0]);
-	
-	/* add list element */
-	for(i=0; i<count; i++) {
-		struct list_node *new = kmalloc(sizeof(struct list_node), GFP_KERNEL);
-		new->data = i;
-		cau_list_add(new, my_list);
-	}
-
-
-	getnstimeofday(&spclock[1]);
-
-	delay = calclock(spclock, &list_time, &list_count);
-	printk("Add for %d times, delay: %llu\n", count, delay);
-
-}
 
 void search_from_linked_list(int count, int num) {
 	struct list_node *my_list = kmalloc(sizeof(struct list_node), GFP_KERNEL);
@@ -85,15 +48,23 @@ void search_from_linked_list(int count, int num) {
 
 	
 	/* add list element */
+	getnstimeofday(&spclock[1]);
+
 	for(i=0; i<count; i++) {
 		struct list_node *new = kmalloc(sizeof(struct list_node), GFP_KERNEL);
 		new->data = i;
 		cau_list_add(new, my_list);
 	}
 	
-	getnstimeofday(&spclock[0]);
+	getnstimeofday(&spclock[1]);
+    
+    delay = calclock(spclock, &list_time, &list_count);
+	printk("Add for %d times, delay: %llu\n", count, delay);
 	
+    
 	/* Search number in list */
+    getnstimeofday(&spclock[0]);
+	
 	cau_list_for_each_entry_safe(current_node, tmp, my_list) {
 		if(current_node->data == num) {
 			printk("found: %d\n", num);
@@ -105,35 +76,12 @@ void search_from_linked_list(int count, int num) {
 
 	delay = calclock(spclock, &list_time, &list_count);
 	printk("Search from %d entries, delay: %llu\n", count, delay);
-}
 
-void remove_from_linked_list(int count) {
-    struct list_node *my_list = kmalloc(sizeof(struct list_node), GFP_KERNEL);
-	struct list_node *tmp;
-	struct list_node *current_node;
-	int i;
-
-	/* Variables for time calcualtion */
-	struct timespec spclock[2];
-	unsigned long long list_time;
-	unsigned long long list_count;
-	unsigned long long delay;	
-	
-	/* Initialize List */
-	CAU_INIT_LIST_HEAD(my_list);
-
-	
-	/* add list element */
-	for(i=0; i<count; i++) {
-		struct list_node *new = kmalloc(sizeof(struct list_node), GFP_KERNEL);
-		new->data = i;
-		cau_list_add(new, my_list);
-	}
-	
-	getnstimeofday(&spclock[0]);
 	
 	/* remove elements from list */
-	cau_list_for_each_entry_safe(current_node, tmp, my_list) {
+    getnstimeofday(&spclock[0]);
+	
+    cau_list_for_each_entry_safe(current_node, tmp, my_list) {
 		cau_list_del(current_node);
 		kfree(current_node);
 	}
@@ -142,7 +90,6 @@ void remove_from_linked_list(int count) {
 
 	delay = calclock(spclock, &list_time, &list_count);
 	printk("Delete for %d times, delay: %llu\n", count, delay);
-
 }
 
 
